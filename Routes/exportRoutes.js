2a97f953d1b749fe7d8c43a551a0ba5f77c565a1
Yasmin123-design <https://github.com/Yasmin123-design/ExportJsonToPdfOrCsv.git>
2025-09-19 14:45:30 +0300

    updateinremovingfile

--- Routes/exportRoutes.js
+++ Routes/exportRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { exportData } from "../Controllers/exportController.js";
-import { deleteFile } from "../controllers/deleteController.js";
+import { deleteFile } from "../Controllers/deleteController.js";
 
 const router = express.Router();
 
