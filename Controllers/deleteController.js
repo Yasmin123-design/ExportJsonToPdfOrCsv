f0fccb6e924ddf61a60f9fed412132e40f4dd906
Yasmin123-design <https://github.com/Yasmin123-design/ExportJsonToPdfOrCsv.git>
2025-09-19 15:01:12 +0300

    updateremovingfiles

--- Controllers/deleteController.js
+++ Controllers/deleteController.js
@@ -9,7 +9,7 @@ export async function deleteFile(req, res) {
         return res.status(400).json({ message: "File name is required" });
     }
 
-    const filePath = path.join(process.cwd(), "exportstopdf", fileName);
+    const filePath = path.join(process.cwd(), "exports", fileName);
 
     if (!fs.existsSync(filePath)) {
         return res.status(404).json({ message: "File not found" });
